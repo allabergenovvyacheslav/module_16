1c54fda1b8f8b8f12d8e6a4ddcdf346e3d869112
Vyacheslav Allabergenov <slawushkin@mail.ru>
2024-11-07 16:40:26 +0300

    Add files via upload

--- module_16_5.py
+++ module_16_5.py
@@ -1,5 +1,37 @@
 # -*- coding: utf-8 -*-
 
+# Задача "Список пользователей в шаблоне":
+
+# Подготовка:
+
+# Используйте код из предыдущей задачи.
+# Скачайте заготовленные шаблоны для их дополнения.
+# Шаблоны оставьте в папке templates у себя в проекте.
+# Создайте объект Jinja2Templates, указав в качестве папки шаблонов - templates.
+
+# Измените и дополните ранее описанные CRUD запросы:
+
+# Напишите новый запрос по маршруту '/':
+# Функция по этому запросу должна принимать аргумент request и возвращать TemplateResponse.
+# TemplateResponse должен подключать ранее заготовленный шаблон 'users.html',
+# а также передавать в него request и список users. Ключи в словаре для передачи определите
+# самостоятельно в соответствии с шаблоном.
+
+# Измените get запрос по маршруту '/user' на '/user/{user_id}':
+# Функция по этому запросу теперь принимает аргумент request и user_id.
+# Вместо возврата объекта модели User, теперь возвращается объект TemplateResponse.
+# TemplateResponse должен подключать ранее заготовленный шаблон 'users.html',
+# а также передавать в него request и одного из пользователей - user. Ключи в словаре для передачи
+# определите самостоятельно в соответствии с шаблоном.
+
+# Создайте несколько пользователей при помощи post запроса со следующими данными:
+# username - UrbanUser, age - 24
+# username - UrbanTest, age - 22
+# username - Capybara, age - 60
+# В шаблоне 'users.html' заготовлены все необходимые теги и обработка условий, вам остаётся только
+# дополнить закомментированные строки вашим Jinja 2 кодом (использование полей id, username и age
+# объектов модели User):
+
 
 from fastapi import FastAPI, Path, HTTPException, Request
 from fastapi.responses import HTMLResponse
@@ -24,9 +56,14 @@ async def get_all(request: Request) -> HTMLResponse:
     return templates.TemplateResponse('users.html', {'request': request, 'users': users})
 
 
-@app.get('/users')
-async def get_users() -> List[User]:
-    return users
+@app.get('/users{user_id}')
+async def get_users(request: Request,
+                    user_id: Annotated[int, Path(
+                        ge=0,
+                        le=100,
+                        description='Enter your user ID',
+                        example='1')]) -> HTMLResponse:
+    return templates.TemplateResponse('users.html', {'request': request, 'user': users[user_id-1]})
 
 
 @app.post('/user/{username}/{age}')
